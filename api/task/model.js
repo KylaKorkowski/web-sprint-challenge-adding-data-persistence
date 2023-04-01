f037f18f273c6d90a98c30165315d5024aac4824
Kyla Korkowski <kekorkowski@gmail.com>
2023-03-31 21:47:05 -1000

    Okay really all are passing now

--- api/task/model.js
+++ api/task/model.js
@@ -1,16 +1,17 @@
 const db = require('../../data/dbConfig')
 
-function getAllTasks() {
-  return db('tasks as t')
-    .select('t.*', 'p.project_name', 'p.project_description')
-    .join('projects as p', 'p.project_id', 't.project_id')
+function find() {
+    return db('tasks as t')
+        .select('t.*', 'p.project_name', 'p.project_description')
+        .join('projects as p', 'p.project_id', 't.project_id')
 }
 
-function createTask(task) {
-  return db('tasks')
-    .insert(task)
-    .returning('*')
-    .then(([newTask]) => newTask)
+function create(task) {
+    return db('tasks')
+        .insert(task)
+        .then(([task_id]) => {
+            return db('tasks').where({ task_id })
+        })
 }
 
-module.exports = { getAllTasks, createTask }
+module.exports = { find, create }
